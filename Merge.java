3d6a907c1aa10e16adc8c893a5289af85064ec55
mmiah00 <mmiah00@stuty.edu>
2019-03-15 14:53:02 -0400

    mergE

--- Merge.java
+++ Merge.java
@@ -4,16 +4,22 @@ public class Merge {
     mergesort (data, 0, data.length -1);
   }
 
-  private static void mergesort (int[] data, int lo, int hi) {
+  private static int[] mergesort (int[] data, int lo, int hi) {
     if (lo >= hi) {
-      return ;
+      return data;
     }
     int mid = hi / 2; //dividing in half
-    mergesort (data, lo, mid);
-    mergesort (data, mid + 1, hi);
-    merge ();
+    return merge (mergesort (data, lo, mid), mergesort (data, mid + 1, hi));
   }
 
-  private static void merge () {
+  private static int[] merge (int [] one, int[] another) {
+    int ans = new int [one.length + another.length];
+    int len = Math.min (one.length, another.length);
+    int index = 0;
+    for (int i = 0; i < len; i ++) {
+      int o = one[i];
+      int a = another[i]; 
+    }
+    return ans;
   }
 }
