f4a938fa4814567d4199a00854e827c3733f72bf
mmiah00 <mmiah00@stuty.edu>
2019-03-15 14:35:34 -0400

    wrote header

---
+++ Merge.java
@@ -0,0 +1,9 @@
+public class Merge {
+  /*sort the array from least to greatest value. This is a wrapper function*/
+  public static void mergesort(int[]data){
+    mergesort (data, 0, data.length -1);
+  }
+
+  private static void mergesort (int[] data, int lo, int hi) {
+  }
+}
