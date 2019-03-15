6420b2911ae1aacab88d28197fe300bea81935da
mmiah00 <mmiah00@stuty.edu>
2019-03-15 14:44:50 -0400

    wrote psuedocode

--- Merge.java
+++ Merge.java
@@ -5,5 +5,15 @@ public class Merge {
   }
 
   private static void mergesort (int[] data, int lo, int hi) {
+    if (lo >= hi) {
+      return ;
+    }
+    int mid = hi / 2; //dividing in half
+    mergesort (data, lo, mid);
+    mergesort (data, mid + 1, hi);
+    merge ();
+  }
+
+  private static void merge () {
   }
 }
