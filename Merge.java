0192820c1ba925f8cebefcfbdb640012a67ed8eb
mmiah <mmiah00@stuy.edu>
2019-03-21 00:57:16 -0400

    took out unnecessary code

--- Merge.java
+++ Merge.java
@@ -14,17 +14,18 @@ public class Merge {
     merge(data, lo, mid, hi);
   }
 
-  private static void merge (int[] data, int lo, int divide, int hi) {
-    int[] left = new int[divide - lo + 1];
+  private static void merge (int[] data, int lo, int mid, int hi) {
+    int[] left = new int[mid - lo + 1];
     for (int a = 0; a < left.length; a ++) {
       left[a] = data[lo+ a];
     }
 
-    int[] right = new int [hi -divide];
+    int[] right = new int [hi -mid];
     for (int b = 0; b < right.length; b ++) {
-      right[b] = data[divide+ b+1 ];
+      right[b] = data[mid+ b+1 ];
     }
 
+    //keeping track index for left and right sides
     int l = 0;
     int r = 0;
     while (l < left.length && r < right.length) {
@@ -39,7 +40,7 @@ public class Merge {
       }
       lo++;
     }
-     //if one is greater than the other, start filling in the trailing values 
+     //if one is greater than the other, start filling in the trailing values
     while (r < right.length) {
       data[lo] = right[r];
       r++;
@@ -51,216 +52,7 @@ public class Merge {
       lo++;
     }
     //System.out.println (toString (data));
-    /*
-    System.out.println (toString (data));
-    int[] left = new int[middle - lo + 1];
-    for (int i = 0; i < left.length; i ++) {
-      left[i] = data[lo + i];
-    }
-    System.out.println (toString (left));
-    int[] right = new int [hi  - middle];
-    for (int i =0 ; i < right.length; i ++){
-      right [i] = data[middle + i + 1];
-    }
-    System.out.println (toString (right));
-
-    int l = 0; //keeping track of left
-    int r = 0; //keeping track of right
-    int i = lo; //keeping track of index in data
-
-    while (i < hi) {
-      if (l < left.length && r < right.length) {
-        if (left[l] <= right[r]) {
-          data[i] = left[l];
-          i ++;
-          l ++;
-        }
-
-        else {
-          data[i] = right[r];
-          i ++;
-          r ++;
-        }
-      }
-      if (l < left.length && r >= right.length) {
-        data[i] = left[l];
-        i ++;
-        l ++;
-      }
-      else {
-        data[i] = right[r];
-        i ++;
-        r ++;
-      }
-    }
-    System.out.println (toString (data));
-    */
    }
-
-  /*
-  private static void merge (int[] data, int lo, int middle, int hi) {
-    System.out.println ("Data: " + toString (data) + "\nlo: " + data[lo] + "\nmiddle: " + data[middle] + "\nend: " + data[hi]);
-    int[] ans = new int[hi - lo + 1];
-    int i = 0; //keeping track of index in data
-    int left = lo; //keeping track of left side
-    int right = middle;
-    while (i < ans.length) {
-      if (left > middle) {
-        for (int x = right; x < ans.length; x ++) {
-          int temp = data[x];
-          ans[i] = temp;
-          i ++;
-        }
-      }
-
-      if (right > hi) {
-        for (int x = left; x < ans.length; x ++) {
-          int temp = data[x];
-          ans[i] = temp;
-          i ++;
-        }
-      }
-
-      if (data[left] <= data[right]) {
-        int temp = data[left];
-        ans[i] = temp;
-        i ++;
-        left ++;
-      }
-      else {
-        int temp = data[right];
-        ans[i] = temp;
-        i ++;
-        right ++;
-      }
-    }
-
-    int index = 0;
-    for (int a = lo; a <= hi; a ++) {
-      data[a] = ans[index];
-      index ++;
-    }
-    System.out.println (data);
-  }
-  */
-
-  /*
-  private static int[] mergesort (int[] data, int lo, int hi) {
-    if (lo >= hi) {
-      int[] ans = {data[lo]};
-      return ans;
-      //return merge (mergesort (data, lo, mid + 1), mergesort (data, mid + 1, hi + 1));
-    }
-    int mid = (lo +hi ) / 2; //dividing in half
-    //return mergesort  (data, lo, mid +1);
-    //mergesort (data, mid + 1, hi + 1);
-    return merge (mergesort (data, lo, mid), mergesort (data,mid + 1, hi));
-  }
-  */
-
-  /*
-  private static int[] merge (int [] one, int[] another) {
-    int[] ans = new int [one.length + another.length ];
-    int i = 0; //keeps track of numbers you are adding to ans
-    int o = 0; //keeps track of index of int[] one
-    int a = 0; //keeps track of index of int[] another
-    while (i < ans.length) {
-      if (one.length == another.length) {
-        if (one[o] <= another[a]) {
-          ans[i] = one[o];
-          i ++;
-          o ++;
-        }
-        else {
-          ans[i] = another[a];
-          i ++;
-          a ++;
-        }
-      }
-      try {
-        //if you reach the end of the shorter list, add the trailing values of the longer list to the answer
-        if (o >= one.length) {
-          for (int x = a; x < ans.length; x ++) {
-            ans[i] = another[x];
-            i ++;
-          }
-        }
-        if (a >= another.length) {
-          for (int x = o; x < ans.length; x ++) {
-            ans[i] = one[x];
-            i ++;
-          }
-        }
-
-        //add the greater value to the answer
-        if (one[o] <= another[a]) {
-          ans[i] = one[o];
-          i ++;
-          o ++;
-        }
-        else {
-          ans[i] = another[a];
-          i ++;
-          a ++;
-        }
-      }
-      catch (IndexOutOfBoundsException e) {
-
-      }
-    }
-
-    return ans;
-  }
-  */
-
-
-
-
-  /*
-  private static void mergesort (int[] data, int lo, int hi) {
-    if (lo >= hi) {
-      return ;
-    }
-    int mid = (lo + hi) / 2;
-    mergesort (data, lo, mid);
-    mergesort (data, mid + 1, hi);
-    merge (data, lo, mid, hi);
-  }
-
-  private static void merge (int[] data, int lo, int middle, int hi) {
-    int i = lo;
-    int left = lo;
-    int right = middle + 1;
-    while (i < hi) {
-      if (left >= middle) {
-        for (int x = right; x < hi ; x ++) {
-          data [i] = data[x];
-          i ++;
-        }
-      }
-
-      if (left >= hi) {
-        for (int x = left; x < middle; x ++) {
-          data[i] = data[x];
-          i ++;
-        }
-      }
-
-      if (data[left] <= data[left]) {
-        data[i] = data[left];
-        i ++;
-        left ++;
-      }
-      else {
-        data[i] = data[left];
-        i ++;
-        left ++;
-      }
-
-    }
-  }
-  */
-
 
   public static String toString (int[] x) {
     String ans = "[";
@@ -303,3 +95,181 @@ public class Merge {
 
   }
 }
+
+/* //extra code for merge
+System.out.println (toString (data));
+int[] left = new int[middle - lo + 1];
+for (int i = 0; i < left.length; i ++) {
+  left[i] = data[lo + i];
+}
+System.out.println (toString (left));
+int[] right = new int [hi  - middle];
+for (int i =0 ; i < right.length; i ++){
+  right [i] = data[middle + i + 1];
+}
+System.out.println (toString (right));
+
+int l = 0; //keeping track of left
+int r = 0; //keeping track of right
+int i = lo; //keeping track of index in data
+
+while (i < hi) {
+  if (l < left.length && r < right.length) {
+    if (left[l] <= right[r]) {
+      data[i] = left[l];
+      i ++;
+      l ++;
+    }
+
+    else {
+      data[i] = right[r];
+      i ++;
+      r ++;
+    }
+  }
+  if (l < left.length && r >= right.length) {
+    data[i] = left[l];
+    i ++;
+    l ++;
+  }
+  else {
+    data[i] = right[r];
+    i ++;
+    r ++;
+  }
+}
+System.out.println (toString (data));
+*/
+/*
+private static void merge (int[] data, int lo, int middle, int hi) {
+  System.out.println ("Data: " + toString (data) + "\nlo: " + data[lo] + "\nmiddle: " + data[middle] + "\nend: " + data[hi]);
+  int[] ans = new int[hi - lo + 1];
+  int i = 0; //keeping track of index in data
+  int left = lo; //keeping track of left side
+  int right = middle;
+  while (i < ans.length) {
+    if (left > middle) {
+      for (int x = right; x < ans.length; x ++) {
+        int temp = data[x];
+        ans[i] = temp;
+        i ++;
+      }
+    }
+
+    if (right > hi) {
+      for (int x = left; x < ans.length; x ++) {
+        int temp = data[x];
+        ans[i] = temp;
+        i ++;
+      }
+    }
+
+    if (data[left] <= data[right]) {
+      int temp = data[left];
+      ans[i] = temp;
+      i ++;
+      left ++;
+    }
+    else {
+      int temp = data[right];
+      ans[i] = temp;
+      i ++;
+      right ++;
+    }
+  }
+
+  int index = 0;
+  for (int a = lo; a <= hi; a ++) {
+    data[a] = ans[index];
+    index ++;
+  }
+  System.out.println (data);
+}
+*/
+
+/*
+private static int[] mergesort (int[] data, int lo, int hi) {
+  if (lo >= hi) {
+    int[] ans = {data[lo]};
+    return ans;
+    //return merge (mergesort (data, lo, mid + 1), mergesort (data, mid + 1, hi + 1));
+  }
+  int mid = (lo +hi ) / 2; //dividing in half
+  //return mergesort  (data, lo, mid +1);
+  //mergesort (data, mid + 1, hi + 1);
+  return merge (mergesort (data, lo, mid), mergesort (data,mid + 1, hi));
+}
+*/
+
+/*
+private static int[] merge (int [] one, int[] another) {
+  int[] ans = new int [one.length + another.length ];
+  int i = 0; //keeps track of numbers you are adding to ans
+  int o = 0; //keeps track of index of int[] one
+  int a = 0; //keeps track of index of int[] another
+  while (i < ans.length) {
+    if (one.length == another.length) {
+      if (one[o] <= another[a]) {
+        ans[i] = one[o];
+        i ++;
+        o ++;
+      }
+      else {
+        ans[i] = another[a];
+        i ++;
+        a ++;
+      }
+    }
+    try {
+      //if you reach the end of the shorter list, add the trailing values of the longer list to the answer
+      if (o >= one.length) {
+        for (int x = a; x < ans.length; x ++) {
+          ans[i] = another[x];
+          i ++;
+        }
+      }
+      if (a >= another.length) {
+        for (int x = o; x < ans.length; x ++) {
+          ans[i] = one[x];
+          i ++;
+        }
+      }
+
+      //add the greater value to the answer
+      if (one[o] <= another[a]) {
+        ans[i] = one[o];
+        i ++;
+        o ++;
+      }
+      else {
+        ans[i] = another[a];
+        i ++;
+        a ++;
+      }
+    }
+    catch (IndexOutOfBoundsException e) {
+
+    }
+  }
+
+  return ans;
+}
+*/
+
+
+
+
+/*
+private static void mergesort (int[] data, int lo, int hi) {
+  if (lo >= hi) {
+    return ;
+  }
+  int mid = (lo + hi) / 2;
+  mergesort (data, lo, mid);
+  mergesort (data, mid + 1, hi);
+  merge (data, lo, mid, hi);
+}
+
+
+}
+*/
